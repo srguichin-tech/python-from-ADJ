87734d7327bcf3443c3e4702b189edf02b44870b
Luis Martinez <sr.guichin@gmail.com>
2025-10-09 14:48:25 -0600

    Agrego archivo for.py

---
+++ for.py
@@ -0,0 +1,10 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> door= False
+>>> if door:
+...     print('enter')
+... else:
+...     print('no enter')
+...
+no enter
+>>>
