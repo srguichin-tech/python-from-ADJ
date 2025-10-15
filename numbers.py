19117a8484f10c1fc1032759a0f456a689a6a07a
Luis Martinez <sr.guichin@gmail.com>
2025-10-15 13:34:17 -0600

    Numbers with python

---
+++ numbers.py
@@ -0,0 +1,34 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> #learning python version3.14 from ADJ
+>>> # SUMA
+>>> 2+2
+4
+>>> 3+4
+7
+>>> 3+(5+3)
+11
+>>> 50- 30
+20
+>>> 50-(30+5)
+15
+>>> # DIVISION Y MULTIPLICACION
+>>> 17/3
+5.666666666666667
+>>> 17/3 # classic division returns a float number
+5.666666666666667
+>>> 17//3 #floor division discards the fractional part
+5
+>>> 17 % 3
+2
+>>> #powers with python its necesary to use ** two times
+>>> 5**2 #
+25
+>>> 2**7
+128
+>>> #assign a variable
+>>> width= 3
+>>> heaight= 3
+>>> width*heaight
+9
+>>>
