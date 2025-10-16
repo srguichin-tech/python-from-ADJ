7a378414ddea5f9081e9779ae2b001bdd23b9db9
Luis Martinez <sr.guichin@gmial.com>
2025-10-15 20:28:38 -0600

    learning list with python

---
+++ list.py
@@ -0,0 +1,24 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> #Strings can be indexed (subscripted), with the first character having index 0.
+>>> word= 'python'
+>>> word [2]
+'t'
+>>> word= '0'
+>>> word [0]
+'0'
+>>> #3.1.3 list
+>>> squares = [1,2,4,6,8,16]
+>>> squares
+[1, 2, 4, 6, 8, 16]
+>>> squares [0]
+1
+>>> squares [-6]
+1
+>>> squares [-1]
+16
+>>> squares [0]
+1
+>>> squares+ [1234]
+[1, 2, 4, 6, 8, 16, 1234]
+>>>
