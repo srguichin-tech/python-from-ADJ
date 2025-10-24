9fafafb2e721829b64cca07dd506a61e00c3bd7b
Luis Martinez <sr.guichin@gmail.com>
2025-10-24 13:54:03 -0600

    cilos sencillos

---
+++ ciclos.py
@@ -0,0 +1,62 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> # Ejercicios con ciclos en Python
+>>> for i in range (9):
+...     print (i)
+...
+0
+1
+2
+3
+4
+5
+6
+7
+8
+>>> for i in range (1,11):
+...     print (i)
+...
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> for i in range (0,20,2):
+...     print('Numero par es:',i)
+...
+Numero par es: 0
+Numero par es: 2
+Numero par es: 4
+Numero par es: 6
+Numero par es: 8
+Numero par es: 10
+Numero par es: 12
+Numero par es: 14
+Numero par es: 16
+Numero par es: 18
+>>> #uilizando el ciclo for
+>>> palabra = 'emmanuel'
+>>> for letra in palabra:
+...     print(letra)
+...
+e
+m
+m
+a
+n
+u
+e
+l
+>>> frutas= ['pera','manzana','uva']
+>>> for fruta in frutas:
+...     print('Me gusta la',fruta)
+...
+Me gusta la pera
+Me gusta la manzana
+Me gusta la uva
+>>>
